f23ba66fc52222f549662510cc55a816de714d7c
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-08-14 21:07:32 -0300

    Adding freezed

---
+++ lib/src/models/user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      username: json['username'] as String,
+      email: json['email'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'username': instance.username,
+      'email': instance.email,
+      'createdAt': instance.createdAt.toIso8601String(),
+      'updatedAt': instance.updatedAt.toIso8601String(),
+    };
