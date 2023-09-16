77eb06106a7b164ef9a478d3427157d36e021464
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-09-16 11:48:05 -0300

    Create follows_view.dart

---
+++ lib/src/modules/follows/follows_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FollowsView extends StatelessWidget {
+  const FollowsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
