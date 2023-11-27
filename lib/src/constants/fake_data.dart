8d2bf3b4d4f6541c4af5dc57728b43b71327e708
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-11-27 15:26:16 -0300

    fix: profile view

--- lib/src/constants/fake_data.dart
+++ lib/src/constants/fake_data.dart
@@ -2,6 +2,69 @@ import 'package:sesoft_uni_mobile/src/models/post.dart';
 import 'package:sesoft_uni_mobile/src/models/profile.dart';
 import 'package:sesoft_uni_mobile/src/models/user.dart';
 
+const fakePostsList = [
+  Post(
+    id: "",
+    content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
+    likesCount: 0,
+    repliesCount: 0,
+    user: User(
+      id: "",
+      username: "leonardooliveira",
+      email: "",
+      profile: Profile(id: "", displayName: "Leonardo Oliveira"),
+    ),
+  ),
+  Post(
+    id: "",
+    content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
+    likesCount: 0,
+    repliesCount: 0,
+    user: User(
+      id: "",
+      username: "leonardooliveira",
+      email: "",
+      profile: Profile(id: "", displayName: "Leonardo Oliveira"),
+    ),
+  ),
+  Post(
+    id: "",
+    content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
+    likesCount: 0,
+    repliesCount: 0,
+    user: User(
+      id: "",
+      username: "leonardooliveira",
+      email: "",
+      profile: Profile(id: "", displayName: "Leonardo Oliveira"),
+    ),
+  ),
+  Post(
+    id: "",
+    content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
+    likesCount: 0,
+    repliesCount: 0,
+    user: User(
+      id: "",
+      username: "leonardooliveira",
+      email: "",
+      profile: Profile(id: "", displayName: "Leonardo Oliveira"),
+    ),
+  ),
+  Post(
+    id: "",
+    content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
+    likesCount: 0,
+    repliesCount: 0,
+    user: User(
+      id: "",
+      username: "leonardooliveira",
+      email: "",
+      profile: Profile(id: "", displayName: "Leonardo Oliveira"),
+    ),
+  ),
+];
+
 const fakePostForOnePostView = Post(
   id: "",
   content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
@@ -13,68 +76,7 @@ const fakePostForOnePostView = Post(
     email: "",
     profile: Profile(id: "", displayName: "Leonardo Oliveira"),
   ),
-  replies: [
-    Post(
-      id: "",
-      content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
-      likesCount: 0,
-      repliesCount: 0,
-      user: User(
-        id: "",
-        username: "leonardooliveira",
-        email: "",
-        profile: Profile(id: "", displayName: "Leonardo Oliveira"),
-      ),
-    ),
-    Post(
-      id: "",
-      content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
-      likesCount: 0,
-      repliesCount: 0,
-      user: User(
-        id: "",
-        username: "leonardooliveira",
-        email: "",
-        profile: Profile(id: "", displayName: "Leonardo Oliveira"),
-      ),
-    ),
-    Post(
-      id: "",
-      content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
-      likesCount: 0,
-      repliesCount: 0,
-      user: User(
-        id: "",
-        username: "leonardooliveira",
-        email: "",
-        profile: Profile(id: "", displayName: "Leonardo Oliveira"),
-      ),
-    ),
-    Post(
-      id: "",
-      content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
-      likesCount: 0,
-      repliesCount: 0,
-      user: User(
-        id: "",
-        username: "leonardooliveira",
-        email: "",
-        profile: Profile(id: "", displayName: "Leonardo Oliveira"),
-      ),
-    ),
-    Post(
-      id: "",
-      content: "Isto é somente para gerar um fake que será utilizado para o Skeletonizer",
-      likesCount: 0,
-      repliesCount: 0,
-      user: User(
-        id: "",
-        username: "leonardooliveira",
-        email: "",
-        profile: Profile(id: "", displayName: "Leonardo Oliveira"),
-      ),
-    ),
-  ],
+  replies: fakePostsList,
 );
 
 final fakeUsersList = [
